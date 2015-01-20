bae66c949ae115a8bb877d340b6299dfaae501cb
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-01-20 23:21:21 +0300

    data provider

--- src/main/java/helpers/DataProviders.java
+++ src/main/java/helpers/DataProviders.java
@@ -3,7 +3,10 @@ import static helpers.Creds.getUser;
 
 public class DataProviders {
     public static Object[][] provideUsers() {
-        Object users[][] = {{"vladimir"}, {"pekarski"}};
+        Object users[][] = {
+                getUser("realcreds"),
+                getUser()
+        };
 
         return users;
     }
