921e4627a02f40b35c322a8e2720cf3043834553
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-01-21 21:10:13 +0300

    phantomjs

--- src/main/java/helpers/Actions.java
+++ src/main/java/helpers/Actions.java
@@ -3,16 +3,13 @@ package helpers;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 
 import java.io.File;
 import java.io.IOException;
 
 import static helpers.WebDriverSingelton.getDiver;
 
-/**
- * Created by asus on 21.01.15.
- */
+
 public class Actions {
 
     public static void takeScreenshot(String path) {
--- src/main/java/helpers/CustomListener.java
+++ src/main/java/helpers/CustomListener.java
@@ -5,6 +5,8 @@ import org.testng.reporters.ExitCodeListener;
 
 import java.util.Date;
 
+import static helpers.Actions.takeScreenshot;
+
 public class CustomListener extends ExitCodeListener {
     @Override
     public void onTestStart(ITestResult result) {
@@ -17,6 +19,7 @@ public class CustomListener extends ExitCodeListener {
     @Override
     public void onTestFailure(ITestResult result) {
         super.onTestStart(result);
+        takeScreenshot("c:/Users/asus/Desktop/screen.png");
         System.out.println(result.getMethod().getTestClass().getName() + " -" +
                 result.getMethod().getMethodName() + " failed at " +
                 new Date());
