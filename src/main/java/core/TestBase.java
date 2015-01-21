018aeebb40f6a6e9313349eaeaecb4056a95ee74
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-01-21 19:43:58 +0300

    Listener

--- src/main/java/core/TestBase.java
+++ src/main/java/core/TestBase.java
@@ -15,6 +15,6 @@ public class TestBase {
 
     @AfterMethod
     public void teardown() {
-        WebDriverSingelton.getDiver().close();
+        WebDriverSingelton.getDiver().quit();
     }
 }
--- src/main/java/helpers/CustomListener.java
+++ src/main/java/helpers/CustomListener.java
@@ -9,24 +9,24 @@ public class CustomListener extends ExitCodeListener {
     @Override
     public void onTestStart(ITestResult result) {
         super.onTestStart(result);
-        System.out.println(result.getMethod().getTestClass().getName() + "-" +
-                result.getMethod().getMethodName() + "started at" +
+        System.out.println(result.getMethod().getTestClass().getName() + " -" +
+                result.getMethod().getMethodName() + " started at " +
                 new Date());
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
         super.onTestStart(result);
-        System.out.println(result.getMethod().getTestClass().getName() + "-" +
-                result.getMethod().getMethodName() + "failed at" +
+        System.out.println(result.getMethod().getTestClass().getName() + " -" +
+                result.getMethod().getMethodName() + " failed at " +
                 new Date());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
         super.onTestSuccess(result);
-        System.out.println(result.getMethod().getTestClass().getName() + "-" +
-                result.getMethod().getMethodName() + "passed at" +
+        System.out.println(result.getMethod().getTestClass().getName() + " -" +
+                result.getMethod().getMethodName() + " passed at " +
                 new Date());
     }
 }
